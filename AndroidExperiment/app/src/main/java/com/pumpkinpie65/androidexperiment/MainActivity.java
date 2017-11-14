5db567b3db579dc2ae19452136d720eb8a3b9704
pumpkinpie65 <pumpkinpie65@live.com>
2017-11-13 22:38:35 -0800

    Target API 27

--- AndroidExperiment/app/src/main/java/com/pumpkinpie65/androidexperiment/MainActivity.java
+++ AndroidExperiment/app/src/main/java/com/pumpkinpie65/androidexperiment/MainActivity.java
@@ -1,8 +1,8 @@
 package com.pumpkinpie65.androidexperiment;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -11,7 +11,7 @@ import com.pumpkinpie65.androidexperiment.modernfeed.ModernFeedActivity;
 import com.pumpkinpie65.androidexperiment.structsandalgos.StructsAndAlgosActivity;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
