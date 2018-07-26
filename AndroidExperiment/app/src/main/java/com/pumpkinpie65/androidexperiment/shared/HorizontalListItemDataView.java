d1d71716533476c9315c0b59d3d3859778377e43
pumpkinpie65 <pumpkinpie65@live.com>
2018-07-25 21:57:36 -0700

    Add padding on HorizontalListItemDataView

--- AndroidExperiment/app/src/main/java/com/pumpkinpie65/androidexperiment/shared/HorizontalListItemDataView.java
+++ AndroidExperiment/app/src/main/java/com/pumpkinpie65/androidexperiment/shared/HorizontalListItemDataView.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.view.Gravity;
 import android.view.ViewGroup;
 
+import com.pumpkinpie65.androidexperiment.R;
+
 /**
  * View for displaying individual data within a horizontal list
  */
@@ -15,6 +17,8 @@ public class HorizontalListItemDataView<T> extends ListItemDataView<T> {
         super(context);
         setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT));
         setGravity(Gravity.CENTER_VERTICAL);
+        int padding = getResources().getDimensionPixelSize(R.dimen.data_view_padding);
+        setPadding(padding, padding, padding, padding);
     }
 
 }
